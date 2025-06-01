4a77f4b7bf9782b53ab28b38c936169eadd4ffa7
CodeCanvX <oleg26052005@icloud.com>
2025-06-01 10:46:50 +0200

    3.0

--- src/main.js
+++ src/main.js
@@ -5,13 +5,6 @@ import Header from "./components/Header/Header.js";
 import Main from "./components/Main/Main.js";
 import Footer from "./components/Footer/Footer.js";
 
-const preloadLink = document.createElement("link");
-preloadLink.rel = "preload";
-preloadLink.as = "image";
-preloadLink.href = new URL("./assets/GAZ.webp", import.meta.url).href;
-preloadLink.type = "image/webp";
-document.head.appendChild(preloadLink);
-
 const app = document.querySelector("#app");
 
 app.appendChild(Nav());
