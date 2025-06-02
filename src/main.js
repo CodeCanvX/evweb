5598f3e41e7d4d9a771a54e4528d9dd826470a3d
CodeCanvX <oleg26052005@icloud.com>
2025-06-02 09:57:28 +0200

    try

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import "./styles/main.scss";
 
-import Nav from "./components/Nav/Nav.js";
-// import Header from "./components/Header/Header.js";
-import Main from "./components/Main/Main.js";
-import Footer from "./components/Footer/Footer.js";
+// import Nav from "./components/Nav/Nav.js";
+// // import Header from "./components/Header/Header.js";
+// import Main from "./components/Main/Main.js";
+// import Footer from "./components/Footer/Footer.js";
 
-const app = document.querySelector("#app");
+// const app = document.querySelector("#app");
 
-app.appendChild(Nav());
-//   app.appendChild(Header());
-app.appendChild(Main());
-app.appendChild(Footer());
+// app.appendChild(Nav());
+// //   app.appendChild(Header());
+// app.appendChild(Main());
+// app.appendChild(Footer());
