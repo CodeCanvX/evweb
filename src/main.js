69df939199025f8a4b1179422924c28c2fad9010
CodeCanvX <oleg26052005@icloud.com>
2025-06-02 10:12:17 +0200

    13

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import "./styles/main.scss";
 // import Nav from "./components/Nav/Nav.js";
 import Header from "./components/Header/Header.js";
 // import Main from "./components/Main/Main.js";
-// import Footer from "./components/Footer/Footer.js";
+import Footer from "./components/Footer/Footer.js";
 
 const app = document.querySelector("#app");
 
 // app.appendChild(Nav());
 app.appendChild(Header());
 // app.appendChild(Main());
-// app.appendChild(Footer());
+app.appendChild(Footer());
